ef8a4806d70b8868de7b0b3c4dea0fa43296dca9
Ivan Tellurian <ivantellurian@Ivans-MacBook-Air.local>
2021-10-10 22:46:01 +0300

    added quicksort.py file with the algo in it

--- quicksort.py
+++ quicksort.py
@@ -1,9 +1,32 @@
 import shutil
-columns = shutil.get_terminal_size().columns()
+columns = shutil.get_terminal_size().columns
+
+def Partitioning(A, start, end):
+	#sort everything less than pivot to left, and everything more than the pivot to the right of the pivot
+	pivot = A[end]
+	pIndex = start
+
+	for i in range(start, end):
+		#all the values bigger than pivot get pushed to the right of the pivot
+		if A[i] <= pivot:
+			A[i], A[pIndex] = A[pIndex], A[i]
+			pIndex += 1
+
+	A[end], A[pIndex] = A[pIndex], A[end]
+
+	return pIndex
 
 def Quicksort(A, start, end):
-	
 
+	if start >= end:
+		return
+
+	pivot = Partitioning(A, start, end)
+
+	#repeat partitioning on sublist containing elements less than the pivot 
+	Quicksort(A, start, pivot-1)
+	#repeat partitioning on sublist containing elements more than the pivot 
+	Quicksort(A, pivot+1, end)
 
 
 
